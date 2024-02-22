19c0ca47d78844238e05c3a05487872b17ebd48c
conpigs <conpigs@gmail.com>
2024-02-21 21:00:48 -0500

    more changes

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -89,8 +89,8 @@ def registration(request):
 
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
+#Update the `get_dealerships` render list of dealerships all by default, particular state if state is passed
 def get_dealerships(request, state="All"):
-# ...
     if(state == "All"):
         endpoint = "/fetchDealers"
     else:
@@ -99,8 +99,8 @@ def get_dealerships(request, state="All"):
     return JsonResponse({"status":200,"dealers":dealerships})
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
-def get_dealer_reviews(request,dealer_id):
-# ...
+def get_dealer_reviews(request, dealer_id):
+    # if dealer id has been provided
     if(dealer_id):
         endpoint = "/fetchReviews/dealer/"+str(dealer_id)
         reviews = get_request(endpoint)
@@ -114,7 +114,6 @@ def get_dealer_reviews(request,dealer_id):
 
 # Create a `get_dealer_details` view to render the dealer details
 def get_dealer_details(request, dealer_id):
-# ...
     if(dealer_id):
         endpoint = "/fetchDealer/"+str(dealer_id)
         dealership = get_request(endpoint)
