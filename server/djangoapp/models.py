54933eff723dbbb6bbf743f00f3dc025f8ed9a37
conpigs <143553407+conpigs@users.noreply.github.com>
2024-02-22 16:13:52 -0500

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,6 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -20,16 +19,10 @@ class CarMake(models.Model):
     def __str__(self):
         return self.name  # Return the name as the string representation
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many
-# Car Models, using ForeignKey field)
-# - Name
-# - Type (CharField with a choices argument to provide limited choices
-# such as Sedan, SUV, WAGON, etc.)
-# - Year (IntegerField) with min value 2015 and max value 2023
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+
 class CarModel(models.Model):
+    
+    
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
     name = models.CharField(max_length=100)
     CAR_TYPES = [
